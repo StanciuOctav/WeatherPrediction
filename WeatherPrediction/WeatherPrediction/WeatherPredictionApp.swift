f2c801bc1b02b449d2a364a91b7ea8bbbbacbfef
StanciuOctav <octav.stanciu00@gmail.com>
2025-02-03 15:20:54 +0100

    Created XCode project

---
+++ WeatherPrediction/WeatherPrediction/WeatherPredictionApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherPredictionApp.swift
+//  WeatherPrediction
+//
+//  Created by Octav Stanciu on 03.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherPredictionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
